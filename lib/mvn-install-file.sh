c5183156241bc7b831c3b5e83cf04bbbd88be451
Peter Brachwitz <peter.brachwitz@gmail.com>
2012-07-31 21:24:13 +0200

    Removed 'call' from shell script

--- lib/mvn-install-file.sh
+++ lib/mvn-install-file.sh
@@ -1,2 +1,2 @@
-call mvn install:install-file -DartifactId=jamon -DgroupId=com.jamonapi -Dversion=2.7 -Dpackaging=jar -Dfile=jamon-2.7.jar
+mvn install:install-file -DartifactId=jamon -DgroupId=com.jamonapi -Dversion=2.7 -Dpackaging=jar -Dfile=jamon-2.7.jar
 
